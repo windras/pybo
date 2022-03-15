2efcbd87a00afa4f08aef7d8c51a847a8a2a8651
bily <windras@naver.com>
2022-03-15 23:40:12 +0900

    로그인/로그아웃 구현하기

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
     'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +123,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,7 @@ from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pybo/', include('pybo.urls'))
+    path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'), # / 페이지에 해당하는 urlpatterns
 ]
