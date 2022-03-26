a7451d4b97c09dd61149cb23816a87b81f885241
bily <windras@naver.com>
2022-03-26 23:15:54 +0900

    질문 수정 기능 추가

--- pybo/views.py
+++ pybo/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def index(request):
@@ -72,6 +73,31 @@ def question_create(request):
     context = {'form': form}
 
     return render(request, 'pybo/question_form.html', {'form': form})
+
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """
+    pybo 질문 수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question_id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()   # 수정일시 저장
+            question.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:
+        form = QuestionForm(instance=question)
+
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
 
 
 def answer_create(request, question_id):
